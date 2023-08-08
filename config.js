dbc709d399b23b43b3980d0a27b91008c5aeca73
serkan-uslu <serkanuslu.info@gmail.com>
2023-08-08 21:51:05 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,4 @@
+export const config = {
+    mainURL: 'https://www.oxfordlearnersdictionaries.com',
+    link: 'https://www.oxfordlearnersdictionaries.com/wordlists/oxford3000-5000',
+}
--- index.js
+++ index.js
@@ -1,59 +1,61 @@
-import puppeteer from "puppeteer";
-import cheerio from "cheerio";
-import fs from "fs";
-import { Parser } from '@json2csv/plainjs';
+import fs from 'fs'
+import cheerio from 'cheerio'
+import puppeteer from 'puppeteer'
+import { config } from './config.js'
+import { Parser } from '@json2csv/plainjs'
 
 export const initFunc = async (config, callback) => {
-    const browser = await puppeteer.launch({
-        "headless": true,
-    });
+    const browser = await puppeteer.launch({})
 
-    const page = await browser.newPage();
+    const page = await browser.newPage()
     await page.goto(config.link, {
-        waitUntil: "domcontentloaded",
-    });
-    const html = await page.evaluate(() => document.body.innerHTML);
-    const $ = cheerio.load(html);
+        waitUntil: 'domcontentloaded',
+    })
+    const html = await page.evaluate(() => document.body.innerHTML)
+    const $ = cheerio.load(html)
 
-    var wordlist = $(".top-g li");
-    var wordlistsCrawl = [];
+    var wordlist = $('.top-g li')
+    var wordlistsCrawl = []
+
     wordlist.each((index, element) => {
         var model = {
-            word: $(element).find("a").text(),
-            pos: $(element).find("span").text(),
-            belong: $(element).find("span.belong-to").text(),
+            word: $(element).find('a').text(),
+            meaning: $(element).find('a').text(),
+            pos: $($(element).find('span')[0]).text(),
+            belong: $(element).find('span.belong-to').text(),
+            pron_uk:
+                config.mainURL +
+                $(element).find('.pron-uk').attr('data-src-ogg'),
+            pron_us:
+                'https://www.oxfordlearnersdictionaries.com' +
+                $(element).find('.pron-us').attr('data-src-ogg'),
         }
-        wordlistsCrawl.push(model);
-    });
+        wordlistsCrawl.push(model)
+    })
 
+    // save JSON
     fs.writeFile('./data.json', JSON.stringify(wordlistsCrawl), function (err) {
-        if (err) throw err;
-        console.log('json Saved!');
-    });
+        if (err) throw err
+        console.log('json Saved!')
+    })
 
-    // convert csv
+    // convert CSV
     try {
-        const parser = new Parser();
-        const csv = parser.parse(wordlistsCrawl);
+        const parser = new Parser()
+        const csv = parser.parse(wordlistsCrawl)
 
         fs.writeFile('./data.csv', csv, function (err) {
-            if (err) throw err;
-            console.log('csv Saved!');
-        });
-
+            if (err) throw err
+            console.log('csv Saved!')
+        })
     } catch (err) {
-        console.error(err);
+        console.error(err)
     }
-    // convert csv
 
-    callback(wordlistsCrawl);
-    await browser.close();
-}
-
-const config = {
-    link: "https://www.oxfordlearnersdictionaries.com/wordlists/oxford3000-5000"
+    callback(wordlistsCrawl)
+    await browser.close()
 }
 
 initFunc(config, function (response) {
-    console.log("bitti")
+    console.log('crawler finished', response)
 })
